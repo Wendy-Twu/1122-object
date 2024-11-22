f251d9018bbc97b5ec1733cb43fe7cc324458f0f
wendy <bossin1030@gmail.com>
2024-11-22 15:47:08 +0800

    靜態宣告

--- index.php
+++ index.php
@@ -1,69 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<h1>物件導向</h1>3
-<?php
-
-class Animal{
-
-public $type='animal';
-public $name='John';
-public $hair_color='black';
-
-function _construct($type,$mame,$hair_color){
-  $this->type=$type;
-  $this->name=$name;
-  $this->hair_color=$hair__color;
-}
-
-function run(){
-    echo $this->name. 'is running';
-}
-
-function speed(){
-    echo $this->name. 'is running at 20km/h';
-}
-public function getName(){
-    return $this->name;
-  } 
-
-  public function setName($name){
-        $this->name=$name;
-  } 
 
 }
-//實例化(instance)
-$cat=new Animal('cat','Kitty','white');
 
-//echo $cat->type;
-echo $cat->getName();
-//echo $cat->hair_color;
-echo $cat->run();
-echo $cat->speed();
-//print_r($cat->feet);
-
-echo $cat->setName('Mary');
-echo $cat->getName();
-
-?>
-<h1>繼承</h1>
-<?php
-
-class Cat extends Animal implements Behavior{
-    protected $type='cat';
-    public $name='Judy';
-    function _construct($hair__color){
-        $this->hair_color=$hair_color;
-    }
-     function jump(){
-        echo $this->name . "jumpping 2m";
-     }
-}
 
 Interface Behavior{
     public function run();
@@ -73,26 +10,66 @@ Interface Behavior{
 
 
 
-
 $mycat=new Cat('white');
 
+print_r($mycat->getFeet());
+
 echo $mycat->getName();
-echo  "<br>";
+echo "<br>";
 echo $mycat->run();
-echo  "<br>";
+echo "<br>";
 echo $mycat->speed();
-echo  "<br>";
-$mycat->getName('judy');
-echo  "<br>";
+echo "<br>";
+$mycat->setName("judy");
+
 echo $mycat->getName();
+echo "<br>";
+echo $mycat->jump();
 
-echo  "<br>";
+//echo Cat::name;
+?>
+
+<H1>靜態宣告</H1>
+<?php
+
+class Dog extends Animal implements Behavior{
+    protected $type='dog';
+    protected $name='Doggy';
+    protected static  $count=0;
+    //static $count=0;
+
+    function __construct($hair_color){
+        $this->hair_color=$hair_color;
+        self::$count++;
+    }
+
+    function bark(){
+        echo $this->name . " is barking";
+    }
+
+    function getFeet(){
+        return $this->feet;
+    }
+
+    static function getCount(){
+        return self::$count;
+    }
+
+    function jump(){
+        echo $this->name . " jumpping 1m";
+    }
+}
+
+echo Dog::getCount();
+
+$dog1=new Dog('brown');
+$dog2=new Dog('black');
+$dog3=new Dog('orange');
+$dog4=new Dog('white');
+$dog5=new Dog('white');
 
 
-
-
-
-
-?>    
+echo Dog::getCount();
+?>
 </body>
 </html>
